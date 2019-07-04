de1cc0ce94a8ac57a40d350972fbf6652fdff973
hirooka <yuy.hirohiro@gmail.com>
2019-07-04 20:20:26 +0900

    RSocket sample

---
+++ rsocket-consumer/src/test/java/com/example/rsocketconsumer/RsocketConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsocketconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rsocket-producer/src/main/java/com/example/rsocketproducer/RsocketProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsocketproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketProducerApplication.class, args);
+	}
+
+}
