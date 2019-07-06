a5537f0d2770806d6aef2557ab4af664257dc15f
hirooka <yuy.hirohiro@gmail.com>
2019-07-06 19:56:54 +0900

    wip

--- rsocket-consumer/src/main/java/com/example/rsocketconsumer/RsocketConsumerApplication.java
+++ rsocket-consumer/src/main/java/com/example/rsocketconsumer/RsocketConsumerApplication.java
@@ -1,13 +1,42 @@
 package com.example.rsocketconsumer;
 
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
 
 @SpringBootApplication
 public class RsocketConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RsocketConsumerApplication.class, args);
+	}
+
+	@Bean
+	RSocket rSocket(){
+		return RSocketFactory
+				.connect()
+				.dataMimeType(MimeTypeUtils.APPLICATION_JSON_VALUE)
+				.frameDecoder(PayloadDecoder.ZERO_COPY)
+				.transport(TcpClientTransport.create(8082))
+				.start()
+				.block();
+	}
+
+	@Bean
+	RSocketRequester requester(RSocketStrategies socketStrategies){
+			return RSocketRequester.wrap(
+					this.rSocket(),
+					MimeTypeUtils.APPLICATION_JSON,
+					MimeTypeUtils.APPLICATION_JSON,
+					socketStrategies);
 	}
 
 }
