51556e05a94374ac858aed5e2d9f3d72d5170180
hirooka <yuy.hirohiro@gmail.com>
2019-07-23 21:23:59 +0900

    first commit

---
+++ fileuplode/src/main/java/com/example/fileuplode/FileuplodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileuplode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuplodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuplodeApplication.class, args);
+	}
+
+}
