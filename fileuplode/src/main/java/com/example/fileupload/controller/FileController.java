9c222728ca32d125b125f15d6de9da0bede903cc
hirooka <yuy.hirohiro@gmail.com>
2019-07-27 00:40:34 +0900

    show in command line

--- fileuplode/src/main/java/com/example/fileupload/controller/FileController.java
+++ fileuplode/src/main/java/com/example/fileupload/controller/FileController.java
@@ -1,37 +1,42 @@
 package com.example.fileupload.controller;
 
-import com.example.fileupload.form.FileForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class FileController {
 
 	@GetMapping("/")
-	public String start(){
+	public String start() {
 		return "upload";
 	}
 
 	@PostMapping("/upload")
-	public String upload(FileForm fileForm){
+	public String upload(@RequestParam("upload_file") MultipartFile multipartFile) {
 
-		MultipartFile multipartFile = fileForm.getFile();
 
 		String contentType = multipartFile.getContentType();
 		String parameterName = multipartFile.getName();
 		String originalFileName = multipartFile.getOriginalFilename();
 
+		System.out.println("originalFileName = " + originalFileName);
+
 		long fileSize = multipartFile.getSize();
 
-		try(InputStream inputStream = multipartFile.getInputStream()){
-			System.out.println("inputStream = " + inputStream.toString());
-		}catch (IOException e){
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+
+			byte[] bytes = inputStream.readAllBytes();
+
+			System.out.println(new String(bytes, StandardCharsets.UTF_8));
+
+		} catch (IOException e) {
 			e.printStackTrace();
 			return "error";
 		}
