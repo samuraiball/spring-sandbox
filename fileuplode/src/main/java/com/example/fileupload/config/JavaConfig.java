c1fb34512fefe742c34a13ba00bb98e478a8fa0c
hirooka <yuy.hirohiro@gmail.com>
2019-07-29 22:13:23 +0900

    add JavaConfig

---
+++ fileuplode/src/main/java/com/example/fileupload/config/JavaConfig.java
@@ -0,0 +1,15 @@
+package com.example.fileupload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+@Configuration
+public class JavaConfig {
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		return new StandardServletMultipartResolver();
+	}
+}
