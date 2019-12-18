19fc00fd0168f969f921f7424f2daa8b98460f5a
yuya hirooka <yuy.hirohiro@gmail.com>
2019-12-19 08:17:30 +0900

    Rsocketの勉強用のチャットサーバfirstcommit

---
+++ rsocket-chat/rsocket-chat-server/src/main/java/dev/hirooka/rsocket/chat/rsocketchatserver/RsocketChatServerApplication.java
@@ -0,0 +1,13 @@
+package dev.hirooka.rsocket.chat.rsocketchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketChatServerApplication.class, args);
+	}
+
+}
---
+++ rsocket-chat/rsocket-chat-server/src/test/java/dev/hirooka/rsocket/chat/rsocketchatserver/RsocketChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hirooka.rsocket.chat.rsocketchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
