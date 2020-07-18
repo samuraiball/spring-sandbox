fb6d46b14f00ae950071791d8bc88ef562581f8e
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-07-18 21:26:51 +0900

    test

---
+++ spring-cloud-contract-polyglot/go-sample/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"hello": "world",
+		})
+	})
+	r.Run(":8083")
+}
