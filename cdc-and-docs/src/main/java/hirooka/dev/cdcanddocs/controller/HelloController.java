8f577af7e54930536d016dbe7171201d0b6cb7b5
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-11-02 23:08:43 +0900

    add samples

--- cdc-and-docs/src/main/java/hirooka/dev/cdcanddocs/controller/HelloController.java
+++ cdc-and-docs/src/main/java/hirooka/dev/cdcanddocs/controller/HelloController.java
@@ -2,6 +2,8 @@ package hirooka.dev.cdcanddocs.controller;
 
 import jdk.jfr.ContentType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,5 +13,14 @@ public class HelloController {
     @GetMapping("/")
     public Map<String, String> hello() {
         return Map.of("hello", "world");
+    }
+
+
+    @PostMapping("/")
+    public Map<String, String> hello(@RequestBody Map<String, Integer> value) {
+        if (value.get("value")>=10) {
+            return Map.of("status", "OK");
+        }
+        return Map.of("status", "NG");
     }
 }
--- cdc-and-docs/src/test/java/hirooka/dev/cdcanddocs/controller/HelloControllerTest.java
+++ cdc-and-docs/src/test/java/hirooka/dev/cdcanddocs/controller/HelloControllerTest.java
@@ -2,31 +2,67 @@ package hirooka.dev.cdcanddocs.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.restdocs.SpringCloudContractRestDocs;
+import org.springframework.cloud.contract.wiremock.restdocs.WireMockRestDocs;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
 class HelloControllerTest {
 
     private MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp(WebApplicationContext context) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    public void setUp(
+            WebApplicationContext context,
+            RestDocumentationContextProvider provider
+    ) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(provider))
+                .build();
     }
 
     @Test
-    void HelloControllerのテスト() throws Exception {
-        mockMvc.perform(get("/"))
+    void HelloControllerのOKテスト() throws Exception {
+        mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"value\": 10}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.hello").value("world"));
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andDo(
+                        WireMockRestDocs.verify()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .jsonPath("$[?(@.value >= 10)]"))
+                .andDo(document("okPattern", SpringCloudContractRestDocs.dslContract()));
+    }
+
+
+    @Test
+    void HelloControllerのNGテスト() throws Exception {
+        mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"value\": 9}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("NG"))
+                .andDo(
+                        WireMockRestDocs.verify()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .jsonPath("$[?(@.value < 10)]"))
+                .andDo(document("ngPattern", SpringCloudContractRestDocs.dslContract()));
     }
 }
