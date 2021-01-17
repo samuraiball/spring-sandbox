c6832eb9470f3a37d10bbdba677d3c6d6999a99b
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2021-01-17 19:59:12 +0900

    init commit for Rsocket sample

---
+++ rsocket/src/test/java/dev/hirooka/rsocket/RsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hirooka.rsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
